3cb294afa547008f557a8e5a93fa0902c8635314
ali <ali.khabbaz14@gmail.com>
2015-07-12 10:09:02 +0430

    init - create collection and read it

---
+++ app.js
@@ -0,0 +1,38 @@
+(function () {
+	'use strict';
+	var Parties = new Mongo.Collection("parties");
+	if (Meteor.isClient) {
+		angular.module('socially', ['angular-meteor']);
+
+		var partiesListCtrl = function ($scope, $meteor) {
+			$scope.parties = $meteor.collection(Parties);
+			console.log('salam');
+		};
+
+		angular.module('socially').controller('partiesListCtrl', partiesListCtrl);
+		partiesListCtrl.$inject = ['$scope', '$meteor'];
+
+
+	}
+
+	if (Meteor.isServer) {
+		Meteor.startup(function () {
+			if (Parties.find().count() === 0) {
+
+				var parties = [{
+					'name': 'Dubstep-Free Zone',
+					'description': 'Can we please just for an evening not listen to dubstep.'
+				}, {
+					'name': 'All dubstep all the time',
+					'description': 'Get it on!'
+				}, {
+					'name': 'Savage lounging',
+					'description': 'Leisure suit required. And only fiercest manners.'
+				}];
+				parties.forEach(function (val) {
+					Parties.insert(val);
+				});
+			}
+		});
+	}
+})();
